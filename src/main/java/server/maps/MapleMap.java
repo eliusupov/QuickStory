a88009edfd2c63d52814f68769819c860b8b2a65
Eli Yusupov <eliusupov123@gmail.com>
2025-09-28 20:59:23 +0300

    .

--- src/main/java/server/maps/MapleMap.java
+++ src/main/java/server/maps/MapleMap.java
@@ -651,6 +651,7 @@ public class MapleMap {
         }
     }
 
+    // real rate
     private byte dropItemsFromMonsterOnMap(List<MonsterDropEntry> dropEntry, Point pos, byte index, int chRate,
                                            byte droptype, int mobpos, Character chr, Monster mob, short delay) {
         if (dropEntry.isEmpty()) {
@@ -723,10 +724,10 @@ public class MapleMap {
 
             // monster card drop rate
             if (ItemConstants.isMonsterCard(de.itemId)) {
-                adjustedChanceMultiplier = 2.5f;
+                adjustedChanceMultiplier = 3f;
 
                 if (mob.isBoss()) {
-                    adjustedChanceMultiplier = 0.25f;
+                    adjustedChanceMultiplier = 1f;
                 }
             }
             
@@ -770,6 +771,127 @@ public class MapleMap {
 
         return index;
     }
+
+    // lower rate
+    // private byte dropItemsFromMonsterOnMap(List<MonsterDropEntry> dropEntry, Point pos, byte index, int chRate,
+    //                                        byte droptype, int mobpos, Character chr, Monster mob, short delay) {
+    //     if (dropEntry.isEmpty()) {
+    //         return index;
+    //     }
+
+    //     Collections.shuffle(dropEntry);
+
+    //     Item idrop;
+    //     ItemInformationProvider ii = ItemInformationProvider.getInstance();
+
+    //     for (final MonsterDropEntry de : dropEntry) {
+    //         float cardRate = chr.getCardRate(de.itemId);
+
+    //         float adjustedChanceMultiplier = 1f;
+
+    //         // equips drop rate
+    //         if (ItemConstants.getInventoryType(de.itemId) == InventoryType.EQUIP) {
+    //             adjustedChanceMultiplier = 3.0f;
+    //             if (mob.isBoss()) {
+    //                 adjustedChanceMultiplier = 0.8f;
+    //             }
+    //         }
+
+    //         // etc drop rate
+    //         if (ItemConstants.getInventoryType(de.itemId) == InventoryType.ETC) {
+    //             if (chr.isEtcDropEnabled()) {
+    //                 adjustedChanceMultiplier = 2f;
+    //             } else {
+    //                 adjustedChanceMultiplier = 0.0f;
+    //             }
+    //         }
+
+    //         if (ItemConstants.isArrow(de.itemId)) {
+    //             adjustedChanceMultiplier = 0f;
+    //         }
+
+    //          // stars/bullets drop rate
+    //          if (ItemConstants.isThrowingStar(de.itemId) || ItemConstants.isBullet(de.itemId)) {
+    //             adjustedChanceMultiplier = 3.6f;
+
+    //             if (mob.isBoss()) {
+    //                 adjustedChanceMultiplier = 1.4f;
+    //             }
+    //         }
+
+    //         // scroll drop rate
+    //         if (de.itemId > 2040000 && de.itemId < 2050000) {
+    //             if (de.itemId != constants.id.ItemId.CHAOS_SCROll_60) {
+    //                 adjustedChanceMultiplier = 7.2f;
+    //                 if (mob.isBoss()) {
+    //                     adjustedChanceMultiplier = 2f;
+    //                 }
+    //             }
+    //         }
+
+    //         // mastery book drop rate
+    //         if (ItemConstants.isMasteryBook(de.itemId)) {
+    //             adjustedChanceMultiplier = 3.0f;
+
+    //             if (mob.isBoss()) {
+    //                 adjustedChanceMultiplier = 1.0f;
+    //             }
+    //         }
+
+    //         // mesos drop rate
+    //         if (de.itemId == 0) {
+    //             adjustedChanceMultiplier = 1.2f;
+    //         }
+
+    //         // monster card drop rate
+    //         if (ItemConstants.isMonsterCard(de.itemId)) {
+    //             adjustedChanceMultiplier = 3f;
+
+    //             if (mob.isBoss()) {
+    //                 adjustedChanceMultiplier = 1f;
+    //             }
+    //         }
+            
+
+    //         int modifiedChance = (int) (de.chance * adjustedChanceMultiplier);
+            
+    //         int dropChance = (int) Math.min((float) modifiedChance * chRate * cardRate, Integer.MAX_VALUE);
+
+    //         if (Randomizer.nextInt(999999) < dropChance) {
+    //             if (droptype == 3) {
+    //                 pos.x = mobpos + ((index % 2 == 0) ? (40 * ((index + 1) / 2)) : -(40 * (index / 2)));
+    //             } else {
+    //                 pos.x = mobpos + ((index % 2 == 0) ? (25 * ((index + 1) / 2)) : -(25 * (index / 2)));
+    //             }
+    //             if (de.itemId == 0) { // meso
+    //                 int mesos = Randomizer.nextInt(de.Maximum - de.Minimum) + de.Minimum;
+
+    //                 if (mesos > 0) {
+    //                     if (chr.getBuffedValue(BuffStat.MESOUP) != null) {
+    //                         mesos = (int) (mesos * chr.getBuffedValue(BuffStat.MESOUP).doubleValue() / 100.0);
+    //                     }
+    //                     mesos = mesos * chr.getMesoRate();
+    //                     if (mesos <= 0) {
+    //                         mesos = Integer.MAX_VALUE;
+    //                     }
+
+    //                     spawnMesoDrop(mesos, calcDropPos(pos, mob.getPosition()), mob, chr, false, droptype,
+    //                             delay);
+    //                 }
+    //             } else {
+    //                 if (ItemConstants.getInventoryType(de.itemId) == InventoryType.EQUIP) {
+    //                     idrop = ii.randomizeStats((Equip) ii.getEquipById(de.itemId));
+    //                 } else {
+    //                     idrop = new Item(de.itemId, (short) 0, (short) (de.Maximum != 1 ? Randomizer.nextInt(de.Maximum - de.Minimum) + de.Minimum : 1));
+    //                 }
+    //                 spawnDrop(idrop, calcDropPos(pos, mob.getPosition()), mob, chr, droptype, de.questid, delay);
+    //             }
+    //             index++;
+    //         }
+    //     }
+
+    //     return index;
+    // }
 
     private byte dropGlobalItemsFromMonsterOnMap(List<MonsterGlobalDropEntry> globalEntry, Point pos, byte d,
                                                  byte droptype, int mobpos, Character chr, Monster mob, short delay) {
